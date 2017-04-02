794ee729af517bc07d8238eb6c38a04bbf51950d
phoenix2909 <sowrabh.sneha@gmail.com>
2017-04-02 14:13:25 +0530

    the second

---
+++ Hello1.java
@@ -0,0 +1,19 @@
+package app1;
+
+
+public class Hello {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		int x=10;
+		int y=20;
+		int result = x+y;
+		int result1 = x*y;
+		int result2 = x%y;
+		System.out.println(result);
+		System.out.println(result1);
+		System.out.println(result2);
+		
+	}
+
+}
