2e6f8c2243efce2e0b22f9e8fa90071bdcfbef5d
Sowrabh Sudhir <sowrabh.sneha@gmail.com>
2017-06-20 20:22:47 +0530

    Sum of Diagonal elements

---
+++ Sumofdiagonalelements.java
@@ -0,0 +1,27 @@
+package app1;
+import java.io.*;
+public class Sumofdiagonalelements{
+	public static void main(String args[]) throws IOException {
+		int d[][] = { { 1, 2}, { 3, 8 }};
+		int k = 0, j = 0;
+		int sum1 = 0, sum2 = 0;
+		for (j = 0; j < d.length; j++) {
+			for (k = 0; k < d.length; k++)
+				System.out.print(d[j][k] + " ");
+			System.out.println();
+		}
+		for (j = 0; j < d.length; j++) {
+			sum1 = sum1 + d[j][j];
+		}
+		k = d.length - 1;
+		for (j = 0; j < d.length; j++) {
+			if (k >= 0) {
+				sum2 = sum2 + d[j][k];
+				k--;
+			}
+		}
+		System.out.println("Sum of Digonal elements are  :" + sum1 + " and "
+				+ sum2);
+	}
+}
+	
